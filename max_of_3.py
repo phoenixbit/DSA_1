5dff5b4fec57b5316b8a00809f33a5371f391d03
phoenix <24x7sumant@gmail.com>
2022-08-22 11:59:15 +0400

    Finding max of three integers

---
+++ max_of_3.py
@@ -0,0 +1,4 @@
+x = int(input("Please input the first integer : "))
+y = int(input("Please input the second integer : "))
+z = int(input("Please input the third integer : "))
+print("The largest of the three integers is : " + str(max(x,y,z)))
